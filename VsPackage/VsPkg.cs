eb78ebdb9cc09bc864f633d7258b887671e9e96d
Joep <joep@yourticketprovider.nl>
2020-12-02 13:54:09 +0100

    remove unused code files

--- VsPackage/VsPkg.cs
+++ VsPackage/VsPkg.cs
@@ -7,12 +7,14 @@
     using Microsoft.VisualStudio.Shell;
 
     [InstalledProductRegistration("#100", "#102", "1.0.0.0")]
-    [Guid(GuidList.guidVSPackagePkgString)]
+    [Guid(guidVSPackagePkgString)]
     [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionExists_string)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [ProvideBindingPath()]
     public sealed class VSPackage : Package
     {
+        public const string guidVSPackagePkgString = "2b621c1e-60a3-48c5-a07d-0ad6d3dd3417";
+
         protected override void Initialize()
         {
             base.Initialize();
