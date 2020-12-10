0822059a3877ec8215f174c7e95334ab2a9a34f2
Joep <joep@yourticketprovider.nl>
2020-12-10 22:01:40 +0100

    add actual extension code

--- DeleteBinObj/DeleteBinObjPackage.cs
+++ DeleteBinObj/DeleteBinObjPackage.cs
@@ -1,12 +1,16 @@
 ﻿namespace DeleteBinObj
 {
     using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
     using System.Runtime.InteropServices;
+    using System.Text.RegularExpressions;
     using System.Threading;
-
+    using EnvDTE;
     using Microsoft.VisualStudio;
     using Microsoft.VisualStudio.Shell;
-
+    using Microsoft.VisualStudio.Shell.Interop;
     using Task = System.Threading.Tasks.Task;
 
     /// <summary>
@@ -36,8 +40,6 @@
         /// </summary>
         public const string PackageGuidString = "7e43adcf-1439-4297-9e1d-8f2e21aee751";
 
-        #region Package Members
-
         /// <summary>
         /// Initialization of the package; this method is called right after the package is sited, so this is the place
         /// where you can put all the initialization code that rely on services provided by VisualStudio.
@@ -50,8 +52,141 @@
             // When initialized asynchronously, the current thread may be a background thread at this point.
             // Do any initialization that requires the UI thread after switching to the UI thread.
             await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+            this.BuildEvents.OnBuildDone += OnBuildDone;
         }
 
-        #endregion
+        void OnBuildDone(vsBuildScope scope, vsBuildAction action)
+        {
+            if (action == vsBuildAction.vsBuildActionClean)
+            {
+                this.DeleteBinOBjFolders(this.DevelopmentToolsEnvironment.Solution);
+            }
+        }
+
+        private void DeleteBinOBjFolders(Solution solution)
+        {
+            this.PaneWriteLine("Delete bin & obj folders started...");
+            this.PaneWriteLine($"Solution: {solution.FileName}");
+
+            var solutionFilePath = Path.GetDirectoryName(solution.FileName);
+            var solutionFileContents = File.ReadAllText(solution.FileName);
+
+            this.GetProjectFilePaths(solutionFileContents, solutionFilePath)
+                .ToList()
+                .ForEach(this.DeleteBinOBjFolders);
+
+            this.PaneWriteLine($"Delete bin & obj folders finished");
+        }
+
+        private void PaneWriteLine(string message)
+        {
+            this.PaneWrite(message + Environment.NewLine);
+        }
+
+        private void PaneWrite(string message)
+        {
+            this.Pane.OutputString(message);
+        }
+
+        IVsOutputWindowPane pane;
+
+        IVsOutputWindowPane Pane
+        {
+            get
+            {
+                if (this.pane == null)
+                {
+                    var outWindow = GetGlobalService(typeof(SVsOutputWindow)) as IVsOutputWindow;
+                    var paneGuid = VSConstants.GUID_BuildOutputWindowPane;
+                    IVsOutputWindowPane generalPane;
+
+                    outWindow.GetPane(ref paneGuid, out generalPane);
+
+                    this.pane = generalPane;
+                }
+
+                return this.pane;
+            }
+        }
+
+        private IEnumerable<string> GetProjectFilePaths(string solutionFileContents, string solutionFilePath)
+        {
+            var projectReferencePattern = "\"(?<project>[^\"]*.csproj)";
+
+            return new Regex(projectReferencePattern)
+                .Matches(solutionFileContents)
+                .Cast<Match>()
+                .Select(m => m.Groups
+                    .Cast<Group>()
+                    .Last()
+                    .Value)
+                .Select(f => {
+                    this.PaneWriteLine($"Project: {solutionFilePath}\\{f}");
+                    return f;
+                })
+                .Select(f => Path.GetDirectoryName(f))
+                .Select(p => $"{solutionFilePath}\\{p}");
+        }
+
+        private void DeleteBinOBjFolders(string projectFilePath)
+        {
+            this.PaneWriteLine($"Project FilePath: {projectFilePath}");
+
+            new[] { "bin", "obj" }
+                .ToList()
+                .ForEach(f => {
+                    Directory.Delete($"{projectFilePath}\\{f}", true);
+                    this.PaneWriteLine($"Folder: {f}: Ok");
+                });
+        }
+
+
+        private BuildEvents BuildEvents
+        {
+            get
+            {
+                if (this.buildEvents == null)
+                {
+                    this.buildEvents = this.DevelopmentToolsEnvironment.Events.BuildEvents;
+                }
+
+                return this.buildEvents;
+            }
+        }
+
+        private BuildEvents buildEvents;
+
+        private DTE developmentToolsEnvironment;
+
+        private DTE DevelopmentToolsEnvironment
+        {
+            get
+            {
+                if (this.developmentToolsEnvironment == null)
+                {
+                    this.developmentToolsEnvironment = this.GetService(typeof(DTE)) as DTE;
+                }
+
+                return this.developmentToolsEnvironment;
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            try
+            {
+                if (disposing)
+                {
+                    this.buildEvents.OnBuildDone -= OnBuildDone;
+
+                    GC.SuppressFinalize(this);
+                }
+            }
+            finally
+            {
+                base.Dispose(disposing);
+            }
+        }
     }
 }
